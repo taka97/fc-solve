e4d20594c0aab0d2294cf7bbe76f458d8a80a060
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2008-07-31 11:38:39 +0000

    Tweaked the parameters in the config.h.cmake to actually make them work.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/branches/conversion-to-cmake@894 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/config.h.cmake
+++ fc-solve/source/config.h.cmake
@@ -18,13 +18,13 @@ extern "C" {
 #cmakedefine COMPACT_STATES
 #cmakedefine INDIRECT_STACK_STATES
 
-#undef CARD_DEBUG_PRES
+#cmakedefine CARD_DEBUG_PRES
 
 /*
  * Define this macro if the C compiler supports the keyword inline or
  * a similar keyword that was found by Autoconf (and defined as inline).
  * */
-#undef HAVE_C_INLINE
+#cmakedefine HAVE_C_INLINE
 
 
 /*
@@ -43,27 +43,27 @@ extern "C" {
     The amount the pack pointers array grows by. Shouldn't be too high
     because it doesn't happen too often.
 */
-#define IA_STATE_PACKS_GROW_BY 32
+#cmakedefine IA_STATE_PACKS_GROW_BY 32
 
 /*
  * The maximal number of Freecells. For efficiency's sake it should be a
  * multiple of 4.
  * */
 
-#define MAX_NUM_FREECELLS 4
+#define MAX_NUM_FREECELLS ${MAX_NUM_FREECELLS}
 
 /*
  * The maximal number of Stacks. For efficiency's sake it should be a
  * multiple of 4.
  * */
 
-#define MAX_NUM_STACKS 10
+#define MAX_NUM_STACKS ${MAX_NUM_STACKS}
 /*
  * The maximal number of initial cards that can be found in a stack.
  * */
-#define MAX_NUM_INITIAL_CARDS_IN_A_STACK 8
+#define MAX_NUM_INITIAL_CARDS_IN_A_STACK ${MAX_NUM_INITIAL_CARDS_IN_A_STACK}
 
-#define MAX_NUM_DECKS 2
+#define MAX_NUM_DECKS ${MAX_NUM_DECKS}
 
 
 #define FCS_STATE_STORAGE_INDIRECT 0
@@ -82,8 +82,8 @@ extern "C" {
 #define FCS_STACK_STORAGE_GLIB_TREE 4
 #define FCS_STACK_STORAGE_GLIB_HASH 5
 
-#undef FCS_STATE_STORAGE
-#undef FCS_STACK_STORAGE
+#define FCS_STATE_STORAGE ${FCS_STATE_STORAGE}
+#define FCS_STACK_STORAGE ${FCS_STACK_STORAGE}
 
 #ifdef __cplusplus
 }
@@ -98,70 +98,31 @@ extern "C" {
 #undef HAVE_LIBGLIB
 
 /* Define to 1 if you have the `m' library (-lm). */
-#undef HAVE_LIBM
+#cmakedefine HAVE_LIBM
 
 /* Define to 1 if you have the `redblack' library (-lredblack). */
 #undef HAVE_LIBREDBLACK
 
-/* Define to 1 if you have the <limits.h> header file. */
-#undef HAVE_LIMITS_H
-
-/* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
-
-/* Define to 1 if you have the `strdup' function. */
-#undef HAVE_STRDUP
-
-/* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
-
-/* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
-
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "${PACKAGE}"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "${PACKAGE_BUGREPORT}"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "${PACKAGE_NAME}"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "${PACKAGE_STRING}"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "${PACKAGE_TARNAME}"
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
-
-/* Define as the return type of signal handlers (`int' or `void'). */
-#undef RETSIGTYPE
-
-/* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define PACKAGE_VERSION "${PACKAGE_VERSION}"
 
 /* Version number of package */
-#undef VERSION
-
-/* Define to empty if `const' does not conform to ANSI C. */
-#undef const
+#define VERSION "${VERSION}"
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
@@ -169,5 +130,3 @@ extern "C" {
 #undef inline
 #endif
 
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-#undef size_t
