c6ffc162fe586c26ab370c5e1b17c8a5616f71c2
Shlomi Fish <shlomif@shlomifish.org>
2018-03-13 20:12:27 +0200

    Attempted tweak.

--- fc-solve/scripts/TEST_OPTIMIZATIONS/cm-mod1.sh
+++ fc-solve/scripts/TEST_OPTIMIZATIONS/cm-mod1.sh
@@ -13,8 +13,8 @@ freecell-solver-range-parallel-solve 1 32000 1 \
 --method random-dfs -to "0123" -dto2 "6,01[2435]=asw(3,20,10,0,1)" -dto2 "13,[0123456j]=asw(1)" -step 500 -sp r:tf --st-name new -nst \
 --method random-dfs -to "[01][2345j]" -dto2 "7,[0123][456j]" -seed 98361 -step 500 -sp r:tf --st-name sD -nst \
 --method random-dfs -to [01][23456j89] -seed 8 -step 500 -sp r:tf --st-name 10 -nst \
---method a-star -asw 0.2,0.8,0,0,0 -step 500 -sp r:tf --st-name 11 -nst \
+--method a-star -asw 0.6,0.8,0,0,0 -to 0123456j8 -dto2 9,012345j -step 500 -sp r:tf --st-name 11 -nst \
 --method a-star -to 012346j -asw 0.5,0,0.3,0,0 -step 500 -sp r:tf --st-name 18 -nst \
 --method random-dfs -seed 105 -step 500 -sp r:tf --st-name 20 -nst \
 --method random-dfs -to "[01][2345j]" -dto2 "8,[0123][456j]" -seed 38693 -step 500 -sp r:tf --st-name sB \
---prelude "245@24,230@5,280@9,412@sI,400@sF,490@sH,428@sG,760@32,380@12,281@24,380@10,76@5,200@new,225@sD,266@sB,380@24,760@20,380@11,760@24,380@11"
+--prelude "100@11,245@24,230@5,280@9,412@sI,400@sF,490@sH,428@sG,380@11,760@32,380@12,281@24,380@10,76@5,200@new,225@sD,266@sB,380@24,760@20,380@11,760@24,380@11"
